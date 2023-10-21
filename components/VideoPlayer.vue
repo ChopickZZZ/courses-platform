3fa434c41facfb7708fa290769eff541512504d6
Альберт Каримов <chopickzzz@gmail.com>
2023-10-22 02:30:09 +0300

    add videoplayer and head title

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  videoId: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <iframe
+    width="460"
+    height="315"
+    title="Video Player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullscreen
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+  >
+  </iframe>
+</template>
--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,29 +1,3 @@
-<template>
-  <p class="mt-0 uppercase font-bold text-sate-400 mb-1">
-    Lesson {{ chapter.number }} - {{ lesson.number }}
-  </p>
-  <h2 class="my-0">{{ lesson.title }}</h2>
-  <div class="flex space-x-4 mt-2 mb-8">
-    <a
-      class="font-normal text-md text-gray-500"
-      :href="lesson.sourceUrl"
-      v-if="lesson.sourceUrl"
-    >
-      Download Source code
-    </a>
-    <a
-      class="font-normal text-md text-gray-500"
-      :href="lesson.downloadUrl"
-      v-if="lesson.downloadUrl"
-    >
-      Download Video
-    </a>
-  </div>
-  <p class="">
-    {{ lesson.text }}
-  </p>
-</template>
-
 <script setup>
 const course = useCourse();
 const route = useRoute();
@@ -39,4 +13,37 @@ const lesson = computed(() => {
     (lesson) => lesson.slug === route.params.lessonSlug
   );
 });
+
+const title = computed(() => {
+  return `${chapter.value.title} - ${lesson.value.title}`;
+});
+
+useHead({ title });
 </script>
+
+<template>
+  <p class="mt-0 uppercase font-bold text-sate-400 mb-1">
+    Lesson {{ chapter.number }} - {{ lesson.number }}
+  </p>
+  <h2 class="my-0">{{ lesson.title }}</h2>
+  <div class="flex space-x-4 mt-2 mb-8">
+    <NuxtLink
+      class="font-normal text-md text-gray-500"
+      :to="lesson.sourceUrl"
+      v-if="lesson.sourceUrl"
+    >
+      Download Source code
+    </NuxtLink>
+    <NuxtLink
+      class="font-normal text-md text-gray-500"
+      :to="lesson.downloadUrl"
+      v-if="lesson.downloadUrl"
+    >
+      Download Video
+    </NuxtLink>
+  </div>
+  <p class="">
+    {{ lesson.text }}
+  </p>
+  <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
+</template>
